a54c6e7179f70c2b577138bf653fb23c51af9474
Shahzaib Ahmed <ahmed@shapesdefined.com>
2023-07-08 13:36:49 +0400

    Docker setup init

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'Joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrdersRepository } from './orders.repository';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { DatabaseModule } from '@app/common';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
