8fb2a62d1b0833a751dce647691a8a5a109ea9b9
shapesdefined <ahmed@shapesdefined.com>
2023-07-10 18:24:50 +0400

    JWT and auth service initialized

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { RabbitmqService } from '@app/common';
 import { AuthModule } from './auth.module';
+import { RmqOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const rabbitmqService = app.get<RabbitmqService>(RabbitmqService);
+  app.connectMicroservice<RmqOptions>(rabbitmqService.getOptions('AUTH', true));
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,52 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    await this.validateCreateUserRequest(createUserDto);
+    return this.usersRepository.save({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  private async validateCreateUserRequest(createUserDto: CreateUserDto) {
+    let user: User;
+    try {
+      user = await this.usersRepository.findOneBy({
+        email: createUserDto.email,
+      });
+    } catch (err) {}
+
+    if (user) {
+      throw new UnprocessableEntityException('Email already exists.');
+    }
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersRepository.findOneBy({ email: email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
+  }
+
+  async getUser(id: number) {
+    return this.usersRepository.findOneBy({ id: id });
+  }
+}
